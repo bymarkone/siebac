d6c74033016d83622c98cb5840558ca61e61b65a
Bruno Trecenti <btrecent@thoughtworks.com>
2015-09-14 12:56:35 -0300

    fixed typo on production deploy script

--- deploy/production.sh
+++ deploy/production.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -i deploy/hosts deploy/play.yml --limit cbsoft.javascript-tookit.com
+ansible-playbook -i deploy/hosts deploy/play.yml --limit cbsoft.javascript-toolkit.com
